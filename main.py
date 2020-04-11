8cfbb255781e3470ca6566424a97726036bf4085
kesha1225 <samedov03@mail.ru>
2020-04-11 18:13:44 +0300

    after review

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import itertools
 import typing
 import random
 import curses
@@ -13,9 +14,11 @@ async def fire(
     canvas: curses.window,
     start_row: typing.Union[float, int],
     start_column: typing.Union[float, int],
+    max_y: int,
+    max_x: int,
     rows_speed: typing.Union[float, int] = -0.3,
     columns_speed: typing.Union[float, int] = 0,
-) -> None:
+):
     """Display animation of gun shot. Direction and speed can be specified."""
 
     row, column = start_row, start_column
@@ -32,12 +35,9 @@ async def fire(
 
     symbol = "-" if columns_speed else "|"
 
-    rows, columns = canvas.getmaxyx()
-    max_row, max_column = rows - 1, columns - 1
-
     curses.beep()
 
-    while 0 < row < max_row and 0 < column < max_column:
+    while 0 < row < max_y and 0 < column < max_x:
         canvas.addstr(round(row), round(column), symbol)
         await asyncio.sleep(0)
         canvas.addstr(round(row), round(column), " ")
@@ -92,51 +92,44 @@ def change_rocket_coordinates(
     return rocket_y, rocket_x
 
 
-async def rocket_flight(canvas: curses.window, max_y: int, max_x: int):
+async def animate_rocket_flight(canvas: curses.window, max_y: int, max_x: int):
     rocket_y = max_y // 2
     rocket_x = max_x // 2
 
-    while True:
+    for frame in itertools.cycle([rocket_frame_1, rocket_frame_2]):
         rocket_y, rocket_x = change_rocket_coordinates(
             canvas, rocket_y, rocket_x, max_y, max_x
         )
-        draw_frame(canvas, rocket_y, rocket_x, rocket_frame_1)
+        draw_frame(canvas, rocket_y, rocket_x, frame)
         await asyncio.sleep(0)
-
-        draw_frame(canvas, rocket_y, rocket_x, rocket_frame_1, negative=True)
-
-        rocket_y, rocket_x = change_rocket_coordinates(
-            canvas, rocket_y, rocket_x, max_y, max_x
-        )
-        draw_frame(canvas, rocket_y, rocket_x, rocket_frame_2)
-        await asyncio.sleep(0)
-
-        draw_frame(canvas, rocket_y, rocket_x, rocket_frame_2, negative=True)
+        draw_frame(canvas, rocket_y, rocket_x, frame, negative=True)
 
 
 def draw(canvas: curses.window):
     canvas.border()
     canvas.nodelay(True)
 
-    rows, columns = canvas.getmaxyx()
+    # https://docs.python.org/3/library/curses.html#curses.window.getmaxyx
+    window_height, window_width = canvas.getmaxyx()
     # rows and columns greater by one then real window size
-    max_y, max_x = rows - 1, columns - 1
+    max_y, max_x = window_height - 1, window_width - 1
 
     curses.curs_set(False)
 
-    stars_max_y = rows - 2
-    stars_max_x = columns - 2
+    # Reducing max dimensions by 2 allows to avoid "curses.error"
+    stars_max_y = window_height - 2
+    stars_max_x = window_width - 2
     coroutines = [
         blink(
             canvas,
-            random.randint(1, stars_max_y),
-            random.randint(1, stars_max_x),
+            random.randint(1, stars_max_y),  # stars mustn't appear on border
+            random.randint(0, stars_max_x),
             delay=random.randint(1, 20),
         )
         for _ in range(random.randint(100, 200))
     ]
 
-    coroutines.append(rocket_flight(canvas, max_y, max_x))
+    coroutines.append(animate_rocket_flight(canvas, max_y, max_x))
 
     while coroutines:
         for coro in coroutines.copy():
